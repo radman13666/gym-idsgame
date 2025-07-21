223a958dd0f55d4a69f65f6f9b5b2f9a25abc7a8
William Kibirango <rwkibirango@gmail.com>
2025-07-21 19:51:39 +0300

    Fixed rendering errors in ppo.py

--- gym_idsgame/envs/util/idsgame_util.py
+++ gym_idsgame/envs/util/idsgame_util.py
@@ -287,10 +287,23 @@ def action_dist_hist(data: np.ndarray,
     if xlims is None:
         xlims = (min(data), max(data))
 
-    sns.histplot(data, kde=True,
-              color = 'darkblue',
-             hist_kws={'edgecolor':'black'},
-             kde_kws={'linewidth': 0.5}, bins=xlims[1], fit=None)
+    # sns.histplot(data, kde=True,
+    #           color = 'darkblue',
+    #          hist_kws={'edgecolor':'black'},
+    #          kde_kws={'linewidth': 0.5}, bins=xlims[1], fit=None)
+    # Plot histogram and KDE
+    sns.histplot(
+        data,
+        kde=True,
+        color='darkblue',
+        edgecolor='black',
+        bins=xlims[1],
+        ax=ax
+    )
+
+    # Manually control KDE line width
+    for line in ax.lines:
+        line.set_linewidth(0.5)
 
     ax.set_xlim(xlims)
     ax.set_xticks(list(range(xlims[1]+1)))
