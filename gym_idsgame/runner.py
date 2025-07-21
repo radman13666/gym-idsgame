c89ed6e5c4c995bedb56f866fe7258c2321e1739
William Kibirango <rwkibirango@gmail.com>
2025-07-21 12:45:11 +0300

    Fixed type error in ppo.py

--- gym_idsgame/runner.py
+++ gym_idsgame/runner.py
@@ -12,6 +12,7 @@ except:
     pass
 
 from gym_idsgame.envs.idsgame_env import IdsGameV21Env
+from gym_idsgame.envs.idsgame_env import IdsGameMinimalDefenseV21Env
 from gym_idsgame.config.client_config import ClientConfig
 from gym_idsgame.config.runner_mode import RunnerMode
 from gym_idsgame.agents.dao.agent_type import AgentType
@@ -113,7 +114,10 @@ class Runner:
         :param config: the training configuration
         :return: trainresult, evalresult
         """
-        env = gym.make(config.env_name, idsgame_config = config.idsgame_config,
+        # env = gym.make(config.env_name, idsgame_config = config.idsgame_config,
+        #                save_dir=config.output_dir + "/results/data/" + str(config.random_seed),
+        #                initial_state_path = config.initial_state_path)
+        env = IdsGameMinimalDefenseV21Env(idsgame_config = config.idsgame_config,
                        save_dir=config.output_dir + "/results/data/" + str(config.random_seed),
                        initial_state_path = config.initial_state_path)
         if config.title is not None:
